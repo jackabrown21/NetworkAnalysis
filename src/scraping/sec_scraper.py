28485e48a948d6a5d8b94ad7ff1de070e99e9bd0
Jack Brown <jackbrown@jacks-mbp.myfiosgateway.com>
2023-07-09 21:20:50 -0400

    Updated scraping mechanism

--- src/scraping/sec_scraper.py
+++ src/scraping/sec_scraper.py
@@ -1,43 +1,33 @@
-import requests
-from xml.etree import ElementTree
-import pandas as pd
+from sec_edgar_downloader import Downloader
+import os
+import xml.etree.ElementTree as ET
 
-
-#Takes list of URLs, yields content of each filing
-def get_filings(filing_urls):
-    for url in filing_urls:
-        response = requests.get(url)
-        if response.status_code == 200:
-            yield response.content
-
-#Takes the context of an XML file and parses for useful information
-def parse_xml(xml_content):
-    root = ElementTree.fromstring(xml_content)
-    for info_table in root.findall('infoTable'):
-        # The "useful information" being extracted
-        yield {
-            'nameOfIssuer': info_table.find('nameOfIssuer').text if info_table.find('nameOfIssuer') is not None else 'Missing',
-            'titleOfClass': info_table.find('titleOfClass').text if info_table.find('titleOfClass') is not None else 'Missing',
-            'cusip': info_table.find('cusip').text if info_table.find('cusip') is not None else 'Missing',
-            'value': info_table.find('value').text if info_table.find('value') is not None else 'Missing',
-            'sshPrnamt': info_table.find('shrsOrPrnAmt/sshPrnamt').text if info_table.find('shrsOrPrnAmt/sshPrnamt') is not None else 'Missing',
-            'investmentDiscretion': info_table.find('investmentDiscretion').text if info_table.find('investmentDiscretion') is not None else 'Missing',
-        }
-
-
-
-
-#Saves data from list of dictionaries to a CSV file with specified filename
-def save_data(data, filename):
-    df = pd.DataFrame(data)
-    df.to_csv(filename, index=False)
+def parse_xml(xml_path):
+    # similar to your existing parse_xml, but operates on a file
+    tree = ET.parse(xml_path)
+    root = tree.getroot()
+    
+    for info_table in root.findall('{http://www.sec.gov/edgar/thirteenffiler}infoTable'):
+        issuer = info_table.find('{http://www.sec.gov/edgar/thirteenffiler}nameOfIssuer').text
+        title = info_table.find('{http://www.sec.gov/edgar/thirteenffiler}titleOfClass').text
+        value = info_table.find('{http://www.sec.gov/edgar/thirteenffiler}value').text
+        print(f"Issuer: {issuer}, Title: {title}, Value: {value}")
 
 def main():
-    filing_urls = [...]
-    data = []
-    for xml_content in get_filings(filing_urls):
-        data.extend(parse_xml(xml_content))
-    save_data(data, 'data.csv')
+    dl = Downloader("data/raw")
+    
+    CIK_SE = "0000807985"  
+    CIK_FMI = "0000764532"  
+    CIK_PC = "0001034524"  
+
+    dl.get("13F-HR", CIK_SE, amount=1)  # get the latest filing
+    dl.get("13F-HR", CIK_FMI, amount=1)  # get the latest filing
+    dl.get("13F-HR", CIK_PC, amount=1)  # get the latest filing
+
+    for root_dir, dirs, files in os.walk("data/raw"):
+        for file in files:
+            if file.endswith('.xml'):
+                parse_xml(os.path.join(root_dir, file))
 
 if __name__ == '__main__':
     main()
