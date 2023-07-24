688582af6906754b3b5add9a52c8883aada6eb31
Jack Brown <jackabrown21@gmail.com>
2023-07-23 22:56:25 -0400

    Added invested company color scale

--- src/analysis/network_builder.py
+++ src/analysis/network_builder.py
@@ -4,11 +4,13 @@ import networkx as nx
 
 # Load the CSV file
 df = pd.read_csv("data/processed/SoutheastenAssetManagement/cash.csv")
-df2 = pd.read_csv("data/processed/PolenCapitalManagement/cashagain.csv")
+df2 = pd.read_csv("data/processed/PolenCapitalManagement/cashagainhardcoded.csv")
+df3 = pd.read_csv("data/processed/FiduciaryManagementInc/cashagainagainhardcoded.csv")
 
 # Define the investing companies
 investing_company = "Southeastern Asset Management"
 new_investing_company = "Polen Capital Management"
+newer_investing_company = "Fiduciary Management Incorporated"
 
 # Create the graph
 G = nx.Graph()
@@ -16,6 +18,7 @@ G = nx.Graph()
 # Add investing companies as nodes
 G.add_node(investing_company)
 G.add_node(new_investing_company)
+G.add_node(newer_investing_company)
 
 # Create a dictionary to hold the invested companies and the investing companies
 investments_dict = {}
@@ -38,17 +41,30 @@ for index, row in df2.iterrows():
     investments_dict[new_invested_company] = investments_dict.get(new_invested_company, [])
     investments_dict[new_invested_company].append(new_investing_company)
 
-# Connect investing companies through common investments
-for invested_company, investing_companies in investments_dict.items():
-    if len(investing_companies) > 1:  # if the company is a common investment
-        for i in range(len(investing_companies) - 1):
-            for j in range(i + 1, len(investing_companies)):
-                G.add_edge(investing_companies[i], investing_companies[j])
+# Iterate over each row in the third dataframe
+for index, row in df3.iterrows():
+    newer_invested_company = row['Name of Issuer'].lower()  # normalize the company name
+    if newer_invested_company not in G.nodes:  # only add the node if it's not already in the graph
+        G.add_node(newer_invested_company)
+    G.add_edge(newer_investing_company, newer_invested_company)
+    investments_dict[newer_invested_company] = investments_dict.get(newer_invested_company, [])
+    investments_dict[newer_invested_company].append(newer_investing_company)
 
 # Create a list of node colors
-colors = ["blue" if node in [investing_company, new_investing_company] 
-          else "red" if G.degree(node) > 1
-          else "grey" for node in G.nodes]
+colors = []
+for node in G.nodes:
+    if node in [investing_company, new_investing_company, newer_investing_company]: 
+        colors.append("blue")
+    else:
+        # count the number of investing companies connected to the node
+        investing_companies_connected = sum(neighbor in [investing_company, new_investing_company, newer_investing_company] 
+                                            for neighbor in G.neighbors(node))
+        if investing_companies_connected == 1:
+            colors.append("grey")
+        elif investing_companies_connected == 2:
+            colors.append("pink")
+        else:  # investing_companies_connected > 2
+            colors.append("red")
 
 # Draw the graph
 nx.draw(G, with_labels=True, node_color=colors, font_size=7)
